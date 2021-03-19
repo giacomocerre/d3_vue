4002408c15fe61fefd2d46a20c5ebd533bb35604
giacomocerre <cerre.giacomo93@gmail.com>
2021-03-19 17:10:07 +0100

    Init D3 first chart in vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>D3 Viz</h1>
+    <svg width="800" height="600" id="viz"></svg>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const d3 = require('d3')
 
-export default {
+export default{
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  mounted() {
+    let numbers = [1110,210,45,650,600,1000];
+    // **** D3 steps ****
+    // select the visual env: Svg
+    const svg = d3.select('#viz');
+
+    // join my data
+    const rects = svg.selectAll('rect')
+      .data(numbers)
+      .join('rect');
+
+    // update: join()
+    const scaleLen = d3.scaleLinear()
+      .domain([0,d3.max(numbers)])
+      .range([0, 600])
+
+    const scalePos = (d, i) => {
+      return 20 * i;
+    }
+
+    rects
+      .attr('x', 20)
+      .attr('height', 19)
+      .attr('y', scalePos)
+      .attr('width', scaleLen)
   }
+
 }
 </script>
 
@@ -21,7 +46,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
