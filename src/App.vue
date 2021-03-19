4f554629e6a9dfd1c90c6687b955cff65577ca3e
giacomocerre <cerre.giacomo93@gmail.com>
2021-03-19 17:33:22 +0100

    Update svg creation

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ export default{
   name: 'App',
   components: {},
   mounted() {
-    let numbers = [1110,210,45,650,600,1000];
+    let numbers = [1110,210,45,650,600,1000,212,345];
     // **** D3 steps ****
     // select the visual env: Svg
     const svg = d3.select('#viz');
@@ -27,15 +27,23 @@ export default{
       .domain([0,d3.max(numbers)])
       .range([0, 600])
 
-    const scalePos = (d, i) => {
-      return 20 * i;
-    }
+    //const scalePos = (d, i) => {
+      //return 30 * i + 20;
+    //}
+
+    const scalePos = d3.scaleBand()
+    .domain(d3.range(numbers.length))
+    .range([0,200])
+    .round({x: true})
+    .paddingInner(0.05)
+    .paddingOuter(0.05)
 
     rects
       .attr('x', 20)
-      .attr('height', 19)
-      .attr('y', scalePos)
+      .attr('height', scalePos.bandwidth())
+      .attr('y', (d,i) => scalePos(i))
       .attr('width', scaleLen)
+      .attr('fill', '#bfa766')
   }
 
 }
