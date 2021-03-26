a19a64da260680bdd9e020b8be4ab22ec839e296
giacomocerre <cerre.giacomo93@gmail.com>
2021-03-26 17:31:49 +0100

    Fix redraw rect on data refresh

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h1>D3 Viz</h1>
+    <button></button>
     <svg width="800" height="600" id="viz"></svg>
   </div>
 </template>
@@ -11,45 +12,65 @@ const d3 = require('d3')
 export default{
   name: 'App',
   components: {},
+  data(){
+    return {
+      numbers: [1110,210,45,650,600,90,213,345]
+    }
+  },
   mounted() {
-    let numbers = [1110,210,45,650,600,1000,212,345];
-    // **** D3 steps ****
-    // select the visual env: Svg
-    const svg = d3.select('#viz');
+    this.refreshChart(this.numbers)
+  },
 
-    const scaleLen = d3.scaleLinear()
-      .domain([0,d3.max(numbers)])
-      .range([0, 600])
+  watch: {
+    numbers(newVal)  {
+      this.refreshChart(newVal);
+    }
+  },
 
-    const lAxis = d3.axisTop(scaleLen);
+  methods: {
+    refreshChart(listOfNums) {
+      // **** D3 steps ****
+      // select the visual env: Svg
+      const svg = d3.select('#viz');
 
-    const scalePos = d3.scaleBand()
-    .domain(d3.range(numbers.length))
-    .range([0,200])
-    .round({x: true})
-    .paddingInner(0.05)
-    .paddingOuter(0.05)
-    // range up un graph
-    svg.append('g')
-        .attr('class', 'lAxis')
-        .attr('transform', 'translate(20, 20)')
-        .call(lAxis);
+      const scaleLen = d3.scaleLinear()
+          .domain([0,d3.max(listOfNums)])
+          .range([0, 600])
 
-    const gs = svg.selectAll('g.bars')// g = elements <g> // .bars = class <g class="bars">
-        .data(numbers)
-        .join('g').attr('class', 'bars');
+      const lAxis = d3.axisTop(scaleLen);
 
-    gs.attr('transform', (d,i) => `translate(20, ${30 + scalePos(i)} )`)
-    // append a <rect> on every <g>
-    gs.append('rect')
-        .attr('fill', '#bfa766')
-        .attr('height', scalePos.bandwidth())
-        .attr('width', scaleLen)
+      const scalePos = d3.scaleBand()
+          .domain(d3.range(listOfNums.length))
+          .range([0,200])
+          .round({x: true})
+          .paddingInner(0.05)
+          .paddingOuter(0.05)
+      // range up on graph
+      svg.append('g')
+          .attr('class', 'lAxis')
+          .attr('transform', 'translate(20, 20)')
+          .call(lAxis);
 
-    gs.append('text')
-        .text((d) => d)
-        .attr('x', scaleLen)
-        .attr('y', scalePos.bandwidth() - 5);
+      const gs = svg.selectAll('g.bars')// g = elements <g> // .bars = class <g class="bars">
+          .data(listOfNums)
+          .join('g').attr('class', 'bars');
+
+      gs.attr('transform', (d,i) => `translate(20, ${30 + scalePos(i)} )`)
+      // append a <rect> on every <g>
+      gs.selectAll('rect')
+          .data(d => [d]) // d is the value of one data
+          .join('rect')
+          .attr('fill', '#bfa766')
+          .attr('height', scalePos.bandwidth())
+          .attr('width', scaleLen)
+
+      gs.selectAll('text')
+          .data(d =>  [d])
+          .join('text')
+          .text((d) => d)
+          .attr('x', scaleLen)
+          .attr('y', scalePos.bandwidth() - 5);
+    }
   }
 
 
